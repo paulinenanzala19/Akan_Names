3c2191df5c2553e693dd12c8d53709d486f7d15c
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-03-05 13:26:02 +0300

    joining multiple arrays

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-var submit=myFunction(){
-    
+var myFunction=function(){
+
+    var daysOfTheWeek =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+    var femaleNames = ["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"]
+    var maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+
 }
