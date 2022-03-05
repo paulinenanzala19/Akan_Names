40e21147d9d001de616009e86764433e188fa8f5
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-03-06 02:23:31 +0300

    adding a query selector to my button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,58 @@
-var myFunction=function(){
+const button=document.querySelector("#submit")
+const genders=document.querySelectorAll("#gender")
 
-    var daysOfTheWeek =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
-    var femaleNames = ["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"]
-    var maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-    var name=(document.getElementById ("name").value)
-    var year=parseInt(document.getElementById("year").value)
-    var month=parseInt(document.getElementById("month").value)
-    var date=parseInt(document.getElementById("date").value)
+const days=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
 
-}
+]
+const maleAkanNames=[
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Koffi",
+    "Kwame"
+]
+const femaleAkanNames=[
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+]
+
+button.addEventListener("click", function(e){
+    e.preventDefault()
+    const userInput=document.querySelector("#year").value
+    
+    let dateOfBirth= new Date(userInput);
+    let day= dateOfBirth.getDay();
+    
+
+    console.log(`the day is:${day}`);
+
+    for (let choice of genders){
+        let gendersChoice= choice.value;
+    
+        if (gendersChoice==="male"){
+            console.log(`Hey your Akan name is ${maleAkanNames[day]} and you were born on ${days[day]}`);
+    
+        } else if (gendersChoice==="female"){
+            console.log(`Hey your Akan name is ${femaleAkanNames[day]} and you were born on ${days[day]}`);
+        } else{
+            alert("please enter your credentials")
+        }
+        
+    }
+
+});
+
