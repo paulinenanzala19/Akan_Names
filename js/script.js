713397979e920c5bae709e543aee213acc43317a
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-03-05 13:33:06 +0300

    identifying variables by id

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@ var myFunction=function(){
     var daysOfTheWeek =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
     var femaleNames = ["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"]
     var maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var name=(document.getElementById ("name").value)
+    var year=parseInt(document.getElementById("year").value)
+    var month=parseInt(document.getElementById("month").value)
+    var date=parseInt(document.getElementById("date").value)
 
 }
